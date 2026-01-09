a42b7d33ee4890995983b467a6608e101a5b14fa
Albert Skriver Frydensberg <s215003@dtu.dk>
2026-01-09 11:47:05 +0100

    model and train
    first model and training setup.

--- make_dataset.py
+++ make_dataset.py
@@ -2,5 +2,9 @@
 import kagglehub, pandas as pd, os
 
 # Download latest version
-path = kagglehub.dataset_download("tawfikelmetwally/wine-dataset")
-df = pd.read_csv(os.path.join(path, os.listdir(path)[0]))
+
+def data_loader():
+    path = kagglehub.dataset_download("tawfikelmetwally/wine-dataset")
+    df = pd.read_csv(os.path.join(path, os.listdir(path)[0]))
+    return df
+
---
+++ model.py
@@ -0,0 +1,12 @@
+#this is the model file
+
+from sklearn.neighbors import KNeighborsClassifier
+
+def create_model(n_neighbors=5):
+    """
+    Create and return a KNN classifier
+    """
+    return KNeighborsClassifier(n_neighbors=n_neighbors)
+
+
+    
