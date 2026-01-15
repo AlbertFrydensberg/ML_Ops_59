488e0a6bef03c2f777615d0e906ac48ff7bf569e
RebeccaMarxen <s214985@dtu.dk>
2026-01-15 13:55:10 +0100

    test in tests for Albert test

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,10 @@
-from ml_ops_59.data import MyDataset
 from torch.utils.data import Dataset
+from ml_ops_59.data import data_loader
 
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset = MyDataset("data/raw")
-    assert isinstance(dataset, Dataset)
+    df = data_loader()
+    assert isinstance(df, Dataset)
+
+
