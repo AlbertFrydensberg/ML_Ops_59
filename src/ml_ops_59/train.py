2e958f6e7ad3c35d246780e5c7760441cd395b90
EmilieMuus <127758485+EmilieMuus@users.noreply.github.com>
2026-01-21 13:16:27 +0100

    ruff check two errors corrected

--- src/ml_ops_59/train.py
+++ src/ml_ops_59/train.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
-import numpy as np
 import hydra
+import numpy as np
 from omegaconf import DictConfig
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedKFold, train_test_split
@@ -12,8 +12,7 @@ from sklearn.preprocessing import StandardScaler
 from ml_ops_59.data import data_loader
 from ml_ops_59.evaluate import compute_confusion_matrix, compute_metrics
 from ml_ops_59.model import create_model
-from ml_ops_59.visualize import plot_confusion_matrix
-from ml_ops_59.visualize import generate_shap_explanations
+from ml_ops_59.visualize import generate_shap_explanations, plot_confusion_matrix
 from ml_ops_59.wandb_logger import WandBLogger
 
 
