6412ff170c92dbe428ee316d2339d242f95a4e37
RebeccaMarxen <s214985@dtu.dk>
2026-01-22 11:07:07 +0100

    problems with git

--- data_drift.py
+++ data_drift.py
@@ -1,17 +1,14 @@
-import os
-
-import kagglehub
 import pandas as pd
-from evidently.legacy.metric_preset import DataDriftPreset
+import kagglehub
+import os
 from evidently.legacy.report import Report
-from sklearn import datasets
+from evidently.legacy.metric_preset import DataDriftPreset
 
 path = kagglehub.dataset_download("tawfikelmetwally/wine-dataset")
 reference_data = pd.read_csv(os.path.join(path, os.listdir(path)[0]))
 
-current_data = pd.read_csv("wine.csv")
-
+current_data = pd.read_csv("data/raw/wine.csv")
 
 report = Report(metrics=[DataDriftPreset()])
-snapshot = report.run(reference_data=reference_data, current_data=current_data)
-snapshot.save_html("report.html")
+report.run(reference_data=reference_data, current_data=current_data)
+report.save_html("report.html")
